f552bc1e16d420198a14be84d736e5ec60f73b52
VincenzoCristallo <113722638+VincenzoCristallo@users.noreply.github.com>
2022-09-17 23:57:35 +0200

    Add files via upload

---
+++ user_generator_module.py
@@ -0,0 +1,75 @@
+
+input('Do you want to start? : press enter key ')
+
+def account_name(): # 1 name example: PiMiner
+    print('Type in the account_name : ')
+    name = input('Name : ')
+    print('This is the Name : ' + name)
+    
+    
+account_name()
+
+def account_id(): # 2 id example: 121
+    print('Type in the account_id : ')
+    id = input('ID : ')
+    print('This is the ID : ' + id)
+    
+    
+account_id()
+
+def account_email(): # 3 email example: gets created automatically tru other function()
+    print('Type in the account_email : ')
+    email = input('Email : ') # account_name + account_id + '@proton.me'    <----<------<-----<----   IMPORTENT!!!!! MAYOR CHANGES NEEDED!!!!!
+    print('This is the Email : ' + email)
+    
+    
+account_email()
+
+def account_password(): # 4 password example: probably a integrated password-generator    <--- MAYOR CHANGES NEEDED!!!!!
+    print('Type in the account_password : ')
+    password = input('Password : ') # function('Function for a password-generator') #     <---<---<----<---<---<---<--- IMPORTENT: VARIABLE NAME HAS TO GET CHANGED!!!!!!
+    print('This is the Password : ' + password)
+    
+    
+#account_password()
+
+def account_role(): # 5 role example: AMB for Ambassador or PIO for Pioneer
+    print('Type in the account_role : ')
+    role = input('Role : ')
+    print('This is the Role : ' + role)
+    
+    
+account_role()
+
+def account_earning_team(): # 6 earning-team example: 1 or 2 or 3 and so on...
+    print('Type in the account_earning_team : ')
+    earning_team = input('Earning-Team : ')
+    print('This is the Earning-Team : ' + earning_team)
+    
+    
+account_earning_team()
+
+def account_sms_verify_num(): # 7 sms verification number example: probably a integrated sms-verification-service
+    print('Type in the account_sms_verify_num : ')
+    sms_verify_num = input('SMS-Verification-Code :')
+    print('This is the SMS-Verification-Code : ' + sms_verify_num)
+    
+    
+account_sms_verify_num()
+
+
+# ---------- End of the user-data-generating part ----------
+
+
+
+
+def account_name(): # 8
+    print('Type in the account_name : ')
+    name = input('Name :')
+    print('This is the name : ' + name)
+    
+    
+#account_name()
+    
+
+input('press the enter key')
